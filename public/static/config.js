6a66a0d008b30c2dbdfc29739ab8714763e9241c
ztyangt <yang2210670@163.com>
2022-07-18 10:39:53 +0800

    修改config.js

--- public/static/config.js
+++ public/static/config.js
@@ -1,9 +1,8 @@
 window.INIS = {
 
     // API地址 - 填写后端域名 请严格按照以下格式填写
-    api: 'https://api.tuixiaozhi.cn/api',
     // api: 'https://inis.ztyang.com/api',
-    // api: 'https://api.inis.cn/api',
+    api: 'https://api.inis.cn/api',
 
     // 后端开启token验证后，需要将token填写到此处
     token: '621f525a3b76f3a5a25e4e765ace5c41',
@@ -11,7 +10,12 @@ window.INIS = {
     // 站点标题
     title: '相左',
 
-    // 路由模式 - true = 带 # 号路由 - 不建议修改此项
+    // 路由模式 - true = 带 # 号路由 - 不建议修改此项 
+    // 注意: 设为false 需要添加以下nginx配置来配合
+    // error_page 404 index.html;
+    // location / {
+    //     try_files $uri $uri/ /index.html;
+    // }
     route_hash: true,
 
     // 请勿擅自修改此处
